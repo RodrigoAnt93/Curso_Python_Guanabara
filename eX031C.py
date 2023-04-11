d746c38c3b7087bbe33f1ab66af7538f46924f5d
RodrigoAnt93 <rodrigoscfc@gmail.com>
2023-04-11 20:29:22 -0300

    Create eX031C.py

---
+++ eX031C.py
@@ -0,0 +1,6 @@
+ins = int(input('Quantas insígnias de Kanto você conquistou? '))
+if ins >= 8:
+    print('Parabéns! Você pode passar e enfrentar a Elite Four. Boa Sorte...')
+else:
+    print('Você ainda não tem insígnias suficiente para enfrentar a Elite Four. Perdão...')
+
