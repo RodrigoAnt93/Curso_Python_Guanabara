5a4476e20a0d836044fb2e1d787ef77e567ed08b
RodrigoAnt93 <rodrigoscfc@gmail.com>
2023-04-10 21:31:44 -0300

    Create Ex016.py

---
+++ Ex016.py
@@ -0,0 +1,7 @@
+import math
+n = float(input('Digite um vaor real: '))
+v = math.ceil(n)
+vf = math.floor(n)
+
+print('O valor digitado foi {} e seu porção inteira é {}. A porção reduzida é {}'.format(n, v, vf))
+
