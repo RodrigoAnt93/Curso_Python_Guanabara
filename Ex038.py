ea25570e3045bdf4d491d94ae5f6e25b91058cab
RodrigoAnt93 <rodrigoscfc@gmail.com>
2023-04-18 21:54:57 -0300

    Create Ex038.py

---
+++ Ex038.py
@@ -0,0 +1,8 @@
+num1 = int(input('Digite um numero inteiro: '))
+num2 = int(input('Digite outro numero inteiro: '))
+if num1 > num2:
+    print('O primeiro numero é o maior.')
+elif num2 > num1:
+    print('O segundo numero é o maior')
+else:
+    print('Os dois numeros são iguais.')
