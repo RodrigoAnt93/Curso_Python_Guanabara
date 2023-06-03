d0afa418292cfb3cc00e93bbe8882d4f5d638ca6
RodrigoAnt93 <rodrigoscfc@gmail.com>
2023-06-02 22:12:02 -0300

    Create Ex096.py

---
+++ Ex096.py
@@ -0,0 +1,11 @@
+def metro_quadrado(med1, med2):
+    med1 * med2
+    print(f'A área de um terreno {med1:.2f}x{med2:.2f} é de {med1 * med2:.2f}m²')
+
+
+m1 = float(input('Largura: '))
+m2 = float(input('Comprimento: '))
+metro_quadrado(m1, m2)
+
+
+
