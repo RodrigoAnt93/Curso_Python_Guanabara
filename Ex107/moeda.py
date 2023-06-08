771b63d4cb2b82de3718b7718ef43242d50948e5
RodrigoAnt93 <rodrigoscfc@gmail.com>
2023-06-07 22:41:50 -0300

    Create moeda.py

---
+++ Ex107/moeda.py
@@ -0,0 +1,18 @@
+def dobrar(n):
+    d = n * 2
+    return d
+
+
+def aumentar(n, t):
+    d = n + (n * t / 100)
+    return d
+
+
+def diminuir(n, t):
+    d = n - (n * t / 100)
+    return d
+
+
+def metade(n):
+    d = n / 2
+    return d
