916a9fe4015697407e6412eda5b387b95eb35444
RodrigoAnt93 <rodrigoscfc@gmail.com>
2023-06-12 13:54:03 -0300

    Create Ex012.py

---
+++ Funções/Ex012.py
@@ -0,0 +1,7 @@
+dia = ('Um', "Dois", "Três", 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez',
+         'Onze', 'Doze', 'Treze', 'Quartoze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte',
+         'Vinte e um', 'Vinte e dois', 'Vinte e três', 'Vinte e quatro', 'Vinte e cinco', 'Vinte e seis', 'Vinte e sete',
+         'Vinte e oito', 'Vinte e nove', 'Trinta', 'Trinta e um')
+rod = '32/08/1993'
+rod = rod.split('/')
+print(rod)
