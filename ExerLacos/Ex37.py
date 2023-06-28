8e68d8b6f1fd9a6c040eb8802c97ecc57f890ec8
RodrigoAnt93 <rodrigoscfc@gmail.com>
2023-06-27 22:12:09 -0300

    Create Ex37.py

---
+++ ExerLacos/Ex37.py
@@ -0,0 +1,5 @@
+div = float(input('Digite o valor da sua dívida: R$'))
+print('Valor da Dívida - Valor do Juros - QNT PARC - Valor da Parcela')
+juros = 0
+for c in range(1, 13, 3):
+    print(f'{div} -  ')
